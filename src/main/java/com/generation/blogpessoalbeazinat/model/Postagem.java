78e4928f264b15f53f5189ae9f3ac640e6a45f52
Beazinat <beatrizgarodrigues@gmail.com>
2024-07-03 13:55:53 -0300

    Métodos GET, POST, PUT  e DELETE

--- src/main/java/com/generation/blogpessoalbeazinat/model/Postagem.java
+++ src/main/java/com/generation/blogpessoalbeazinat/model/Postagem.java
@@ -22,14 +22,14 @@ public class Postagem {
 	
 	@NotBlank(message = "O atributo título é Obrigatório!")
 	@Size(min = 5, max = 100, message = "O atributo título deve conter no mínimo 05 e no máximo 100 caracteres")
-	private String title;
+	private String titulo;
 	
 	@NotBlank(message = "O atributo texto é Obrigatório!")
 	@Size(min = 10, max = 1000, message = "O atributo texto deve conter no mínimo 10 e no máximo 1000 caracteres")
-	private String text;
+	private String texto;
 	
 	@UpdateTimestamp
-	private LocalDateTime date;
+	private LocalDateTime data;
 
 	public Long getId() {
 		return id;
@@ -39,29 +39,28 @@ public class Postagem {
 		this.id = id;
 	}
 
-	public String getTitle() {
-		return title;
+	public String getTitulo() {
+		return titulo;
 	}
 
-	public void setTitle(String title) {
-		this.title = title;
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
 	}
 
-	public String getText() {
-		return text;
+	public String getTexto() {
+		return texto;
 	}
 
-	public void setText(String text) {
-		this.text = text;
+	public void setTexto(String texto) {
+		this.texto = texto;
 	}
 
-	public LocalDateTime getDate() {
-		return date;
+	public LocalDateTime getData() {
+		return data;
 	}
 
-	public void setDate(LocalDateTime date) {
-		this.date = date;
+	public void setData(LocalDateTime data) {
+		this.data = data;
 	}
-	
 
 }
